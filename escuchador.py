05b0e158c5745a93de3589cba1f813513315756a
benjasavanco <105219053+benjasavanco@users.noreply.github.com>
2026-05-13 00:58:13 -0300

    escuchador.py

--- escuchador.py
+++ escuchador.py
@@ -112,6 +112,5 @@ def recibir_mensaje():
     return jsonify({"status": "success"}), 200
 
 if __name__ == '__main__':
-    # Puerto 5000 para local, Railway usará la variable de entorno PORT automáticamente
     port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=port)
